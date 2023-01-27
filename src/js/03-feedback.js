ff5a7c02b789d17b0b010285eb0f7f0bb0cc6cc4
Dobrobatko Dmytro <dobrobatka@gmail.com>
2023-01-27 23:30:18 +0200

    add tasks 2,3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('.feedback-form input');
+
+populateForm();
+
+const localObject = {
+  email: '',
+  message: '',
+};
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(localObject);
+}
+form.addEventListener('input', throttle(addLocalStorage, 500));
+
+function addLocalStorage(event) {
+  localObject[event.target.name] = event.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(localObject));
+}
+
+function populateForm() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  if (savedMessage) {
+    textarea.value = JSON.parse(savedMessage).message;
+    input.value = JSON.parse(savedMessage).email;
+  }
+}
